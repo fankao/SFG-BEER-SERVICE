17dc824e516ada023c1842ed69becc393fc0ce2f
chien <minhchiendc@gmail.com>
2020-11-14 09:14:13 +0700

    Adding bootstrap for beer

--- src/main/java/com/spring/beer/sfgbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/com/spring/beer/sfgbeerservice/web/model/BeerStyleEnum.java
@@ -1,4 +1,7 @@
 package com.spring.beer.sfgbeerservice.web.model;
 
+import javax.persistence.AssociationOverride;
+
 public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
