d6a6abac752cd00db90ca089b78e88ee03c37117
chien <minhchiendc@gmail.com>
2020-11-15 11:05:01 +0700

    Add error handler

--- src/main/java/com/spring/beer/sfgbeerservice/web/controller/BeerController.java
+++ src/main/java/com/spring/beer/sfgbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.spring.beer.sfgbeerservice.web.controller;
 import com.spring.beer.sfgbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -12,20 +13,19 @@ import java.util.UUID;
 public class BeerController {
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
         return ResponseEntity.ok(BeerDto.builder().id(beerId).build());
     }
+
     @PostMapping
-    public ResponseEntity<?> saveNewBeer(@RequestBody BeerDto beer){
+    public ResponseEntity<?> saveNewBeer(@RequestBody @Validated BeerDto beer) {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<?> updateBeerById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beer){
+    public ResponseEntity<?> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beer) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
 
 
 }
